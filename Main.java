232e680086ef718daf804c608ee0c252b86f4d0b
Jéssica Almeida de Oliveira <32886156+jessicaalmeidadeoliveiradede@users.noreply.github.com>
2023-03-31 14:39:17 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,47 @@
+public class cliente {
+public void main(String); {
+  char nome ;
+    printf("escreva o nome do cliente ");
+    int idade;
+    printf("escreva a idade do cliente ");
+    int data_nacimento;
+    printf("qual e a sua data de nacimento do clinte ");
+      int cpf;
+    printf("qual e o seu CPF");
+
+};
+};
+
+
+  public class vendas{
+    public void main(String);{
+       float id_vendas;
+        printf("qual e o id_vendas");
+        int data_vendas;
+        printf("qual e a data_vendas");
+       float  id_cliente ;
+        printf("qual e o id_cliente ");
+         int idade ;
+        printf("quel e a idade do cliente ");
+        char produto_comprado;
+        printf("quais são os prdutos_conprados ");
+         float valor ;
+        printf("qual e o valor do produto_conprado");
+           
+    };
+        
+    };
+      public class produto {
+    public void main(String);{
+         float   preco;
+        printf("quanto preco este produto");
+        float desconto;
+        printf("este produto tera desconto");
+         int idade;
+        printf("qual e sua idade");
+         int  desconto_idade;
+        printf("qual e a sua desconto_idade ");
+    };
+      
+      };
+      
